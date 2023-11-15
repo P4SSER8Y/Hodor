98266d3292fecb66408e4454c0ca3e3d75c63de2
P4SSER8Y <P4SSER8Y@outlook.com>
2023-11-16 02:17:33 +0800

    ENH: use nav instead tab

--- door/components/Winterfell.vue
+++ door/components/Winterfell.vue
@@ -1,23 +1,42 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import Bran from './Bran.vue';
 import Hodor from './Hodor.vue';
+
+const who = [ref(true), ref(false)];
+function activate(id: number) {
+  who.forEach(x => x.value = false);
+  who[id].value = true;
+}
 </script>
 
 <template>
-  <div class="card w-96 shadow-2xl card-bordered">
+  <div class="card w-80 shadow-2xl card-bordered">
     <div class="card-body w-full">
-      <div class="tabs tabs-bordered tabs-lg">
-        <input type="radio" name="tab" class="tab" aria-label="HODOR" checked />
-        <div class="tab-content">
-          <Hodor></Hodor>
-        </div>
-        <input type="radio" name="tab" class="tab" aria-label="BRAN" />
-        <div class="tab-content">
-          <Bran></Bran>
-        </div>
+      <div v-if="who[0].value">
+        <Hodor></Hodor>
+      </div>
+      <div v-if="who[1].value">
+        <Bran></Bran>
       </div>
     </div>
   </div>
+  <div class="btm-nav btm-nav-lg">
+    <button :class="who[0].value ? 'is-active' : 'is-inactive'" @click="activate(0)">
+      HODOR
+    </button>
+    <button :class="who[1].value ? 'is-active' : 'is-inactive'" @click="activate(1)">
+      BRAN
+    </button>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="postcss">
+.is-active {
+  @apply active font-bold;
+}
+
+.is-inactive {
+  @apply text-secondary;
+}
+</style>
