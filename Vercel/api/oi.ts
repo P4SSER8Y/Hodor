3f5f85ef06d74ce6432f3843510cb7d05a32fc1b
P4SSER8Y <P4SSER8Y@outlook.com>
2025-05-28 23:19:07 +0800

    ENH: api/oi return details

--- Vercel/api/oi.ts
+++ Vercel/api/oi.ts
@@ -1,11 +1,23 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { kv } from "@vercel/kv";
 
+interface Result {
+  status: string;
+  last: string;
+  now: string;
+}
+
 export default async function (
   request: VercelRequest,
   response: VercelResponse
 ) {
   let now = new Date().toISOString();
-  await kv.set("ping", now);
-  response.status(200).send(now);
+  let last = await kv.get<string>("ping");
+  let t = await kv.set("ping", now);
+  let result: Result = {
+    status: t ? "ok": "error",
+    last: last ?? "",
+    now: now,
+  };
+  response.status(200).json(result);
 }
